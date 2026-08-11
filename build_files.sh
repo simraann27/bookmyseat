9fd46664e5afdfa465299434f13268811a51cc8b
simraann27 <simran.manisha27@gmail.com>
2026-08-11 21:36:03 +0530

    Configure build_files.sh and vercel.json distDir for staticfiles output

---
+++ build_files.sh
@@ -0,0 +1,5 @@
+echo "Installing dependencies..."
+python3 -m pip install -r requirements.txt
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput --clear
+echo "Build complete!"
