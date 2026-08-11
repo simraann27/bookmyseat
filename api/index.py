cffb20c960be2ab885282f649bd35c7b938be2d1
simraann27 <simran.manisha27@gmail.com>
2026-08-11 21:29:02 +0530

    Add Vercel serverless api/index.py entrypoint and clean build config

---
+++ api/index.py
@@ -0,0 +1,1 @@
+from bookmyseat.wsgi import app
--- build_files.sh
+++
@@ -1,4 +0,0 @@
-echo "Building project dependencies..."
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
-echo "Build complete!"
