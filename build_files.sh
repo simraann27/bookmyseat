6d906c8a00db4557b30b0ae5626830a68110fd51
simraann27 <simran.manisha27@gmail.com>
2026-08-11 21:45:46 +0530

    Remove legacy static-build configuration and build_files.sh for pure serverless Python deployment

--- build_files.sh
+++
@@ -1,5 +0,0 @@
-echo "Installing dependencies..."
-python3 -m pip install -r requirements.txt
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear
-echo "Build complete!"
